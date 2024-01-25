d9428b741b5a4f20b610ddd5e4c1149a82011b19
AndrewPrymenko <ffeelex@gmail.com>
2024-01-25 16:51:12 +0100

    #Obtaining the longest binary gap

---
+++ Cycles.Library/Lesson1/BinaryGap.cs
@@ -0,0 +1,36 @@
+﻿namespace Cycles.Library.Lesson1;
+
+public class BinaryGap
+{
+    /// <summary>
+    /// Obtaining the longest binary gap.
+    /// </summary>
+    /// <param name="N">Input parameter to convert to binary number system.</param>
+    /// <returns><see cref="int"/></returns>
+    public static int GetLongestBinaryGap(int N)
+    {
+        var longestBinaryGap = 0;
+        var i = 0;
+        var isFirstLsbZero = (N & 1) == 0;
+        while (N > 0)
+        {
+            var leastSignificantByte = (N & 1) == 0;
+            N >>= 1;
+            if (isFirstLsbZero && leastSignificantByte)
+                continue;
+            else if (leastSignificantByte)
+            {
+                i++;
+                longestBinaryGap = i > longestBinaryGap
+                                    ? i
+                                    : longestBinaryGap;
+            }
+            else
+            {
+                isFirstLsbZero = false;
+                i = 0;
+            }
+        }
+        return longestBinaryGap;
+    }
+}
---
+++ Cycles.Test/GlobalUsings.cs
@@ -0,0 +1,2 @@
+global using Cycles.Library.Lesson1;
+global using NUnit.Framework;
