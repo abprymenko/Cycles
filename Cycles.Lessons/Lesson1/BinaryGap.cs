a12bd16580267a6be0aa5041f6a07e5f48063e95
Andrii Prymenko <127588413+abprymenko@users.noreply.github.com>
2024-01-29 10:01:05 +0100

    Update BinaryGap.cs
    Removed first variant

--- Cycles.Lessons/Lesson1/BinaryGap.cs
+++ Cycles.Lessons/Lesson1/BinaryGap.cs
@@ -1,4 +1,4 @@
-﻿namespace Cycles.Lessons.Lesson1;
+namespace Cycles.Lessons.Lesson1;
 
 #region Usings
 using Cycles.Extensions;
@@ -9,38 +9,11 @@ public class BinaryGap
 {
     #region Public : Methods
     /// <summary>
-    /// Obtaining the longest binary gap.
-    /// </summary>
-    /// <param name="N">Input parameter to convert to binary number system.</param>
-    /// <returns><see cref="int"/></returns>
-    public static int GetLongestBinaryGap(int N)
-    {
-        var longestBinaryGap = 0;
-        var isFirstLsbZero = (N & 1) == 0;
-        for (var i = 0; N > 0; N >>= 1)
-        {
-            if (isFirstLsbZero && (N & 1) == 0)
-                continue;
-            else
-            {
-                if ((N & 1) == 0)
-                {
-                    i++;
-                    longestBinaryGap = Math.Max(longestBinaryGap, i);
-                    continue;
-                }
-                isFirstLsbZero = false;
-                i = 0;
-            }
-        }
-        return longestBinaryGap;
-    }
-    /// <summary>
     /// Obtaining the longest binary gap by using <seealso cref="Queue{T}"/>.
     /// </summary>
     /// <param name="N">Input parameter to convert to binary number system.</param>
     /// <returns><see cref="int"/></returns>
-    public static int GetLongestBinaryGapQueue(int N)
+    public static int LongestBinaryGap(int N)
     {
         int i = 0, longestBinaryGap = 0;
         var bytes = N.ToBinary();
