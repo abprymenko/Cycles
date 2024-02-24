3ca115c1c1a1a7e2465d2c35a140a1acc099a16c
AndrewPrymenko <ffeelex@gmail.com>
2024-02-24 14:28:05 +0100

    #added factorial async

---
+++ Cycles.Helpers/AsynchronousRecursion/Factorial.cs
@@ -0,0 +1,13 @@
+﻿namespace Cycles.Helpers.AsynchronousRecursion;
+
+#region Factorial
+public class Factorial
+{
+    public static async Task<ulong> FactorialAsync(ulong number)
+    {
+        if (number == 0)
+            return 1;
+        return number * await FactorialAsync(number - 1);
+    }
+}
+#endregion
