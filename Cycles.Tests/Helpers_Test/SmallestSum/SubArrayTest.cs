f081085eff30d1d9456d05e3c4c04906e43a2563
Andrii Prymenko <127588413+abprymenko@users.noreply.github.com>
2024-04-27 20:35:06 +0200

    Update SubArrayTest.cs

--- Cycles.Tests/Helpers_Test/SmallestSum/SubArrayTest.cs
+++ Cycles.Tests/Helpers_Test/SmallestSum/SubArrayTest.cs
@@ -1,4 +1,4 @@
-﻿namespace Cycles.Tests.Helpers_Test.SmallestSum;
+namespace Cycles.Tests.Helpers_Test.SmallestSum;
 
 #region SubArrayTest
 internal class SubArrayTest
@@ -19,7 +19,7 @@ internal class SubArrayTest
     [TestCase(new int[]{ 1, 2, 3, 4, 5 })]
     [TestCase(new int[] { 2, 4, -2, 6, -9, 7, -3 })]
     [TestCase(new int[] { 0, 0, 0, 0, 0 })]
-    public void MinLength0_Test(int[] array)
+    public void MinLengthTest0(int[] array)
     {
         var act = SubArray.MinLength(array);
         Assert.That(act, Is.EqualTo(_expected[0]));
@@ -27,21 +27,21 @@ internal class SubArrayTest
     [Test]
     [TestCase(new int[] { 4, 3, 2, -3, 0, -2, 1, -1, -2 })]
     [TestCase(new int[] { 1, 2, -2, 4, -1, 0 })]
-    public void MinLength2_Test(int[] array)
+    public void MinLengthTest2(int[] array)
     {
         var act = SubArray.MinLength(array);
         Assert.That(act, Is.EqualTo(_expected[1]));
     }
     [Test]
     [TestCase(new int[] { 4, 2, 2, -3, 1, 0, 4 })]
-    public void MinLength3_Test(int[] array)
+    public void MinLengthTest3(int[] array)
     {
         var act = SubArray.MinLength(array);
         Assert.That(act, Is.EqualTo(_expected[2]));
     }
     [Test]
     [TestCase(new int[] { 2, 0, 0, 1, -3 })]
-    public void MinLength5_Test(int[] array)
+    public void MinLengthTest5(int[] array)
     {
         var act = SubArray.MinLength(array);
         Assert.That(act, Is.EqualTo(_expected[3]));
