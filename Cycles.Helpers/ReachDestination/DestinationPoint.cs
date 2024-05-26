4adfbcd85acdfa23381aa3a8d7e3b9e7dfbbfe01
Andrii Prymenko <127588413+abprymenko@users.noreply.github.com>
2024-05-27 00:15:48 +0200

    Update DestinationPoint.cs

--- Cycles.Helpers/ReachDestination/DestinationPoint.cs
+++ Cycles.Helpers/ReachDestination/DestinationPoint.cs
@@ -1,19 +1,6 @@
-﻿namespace Cycles.Helpers.ReachDestination;
+namespace Cycles.Helpers.ReachDestination;
 
 #region DestinationPoint
-/*
- *You are given the array paths, where paths[i] = [pointAi, pointBi] means there exists a direct path going from pointAi to pointBi.
- *Return the destination point, that is, the point without any path outgoing to another point.
- *It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination point.
- *
- *Example 1:
- *Input: paths = [["PointA","PointB"],["PointB","PointC"], ["PointC", "PointD"]]
- *Output: "PointD"
- *
- *Explanation: Starting at "PointA" point you
- *will reach "PointD" point which is the destination point. Your trip consist of:
- *"PointA" -> "PointB" -> "PointC" -> "PointD"
- */
 public static class DestinationPoint
 {
     #region Public : Methods
@@ -21,7 +8,7 @@ public static class DestinationPoint
     ///     Gets destination point.
     /// </summary>
     /// <param name="paths"></param>
-    /// <returns></returns>
+    /// <returns>The destination point, that is, the point without any path outgoing to another point.</returns>
     public static T GetDestinationPoint<T>(T[,] paths)
     {
         var outs = new HashSet<T>();
